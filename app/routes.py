f865876fa2a2f17087d5333510ed7659c3767e94
Jeff Somers <jeff.somers@kbc.be>
2023-03-08 20:53:33 +0100

    first version

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from app import app
+import os
+from flask import render_template
+from flask import send_from_directory
 
-@app.route('/')
-@app.route('/index')
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),'favicon.ico'
+    )
+
+@app.route("/")
+@app.route("/index")
 def index():
-    return "Hello, World!"
+    return render_template("base.html")
---
+++ app/static/css/main.js
@@ -0,0 +1,17 @@
+/**
+ *
+ *  sticky navigation
+ *
+ */
+
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+  // get the complete hight of window
+  //let oTop = window.innerHeight - $(".section-2").offset().top;
+  if ($(window).scrollTop() > $(".section-1").offset().top) {
+    navbar.addClass("sticky");
+  } else {
+    navbar.removeClass("sticky");
+  }
+});
