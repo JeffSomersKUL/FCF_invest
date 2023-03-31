523f564fd178131d891d7a406f6e6541da1d263c
Jeff Somers <jeff.somers@kbc.be>
2023-03-31 16:50:20 +0200

    changes for feacture cards

--- app/static/css/main.js
+++ app/static/css/main.js
@@ -9,7 +9,7 @@ let navbar = $(".navbar");
 $(window).scroll(function () {
   // get the complete hight of window
   //let oTop = window.innerHeight - $(".section-2").offset().top;
-  if ($(window).scrollTop() > $(".section-1").offset().top) {
+  if ($(window).scrollTop() >= $(".section-1").offset().top) {
     navbar.addClass("sticky");
   } else {
     navbar.removeClass("sticky");
