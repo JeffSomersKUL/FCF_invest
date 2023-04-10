c222acd183e714516c2d6d3ae5b1b77e4bc02890
Jeff Somers <jeff.somers@kbc.be>
2023-04-11 00:36:45 +0200

    new interactive feeling

--- app/static/css/main.js
+++ app/static/css/main.js
@@ -6,12 +6,20 @@
 
 let navbar = $(".navbar");
 
+let stats = $(".section-2 .container-fluid")
+
 $(window).scroll(function () {
   // get the complete hight of window
   //let oTop = window.innerHeight - $(".section-2").offset().top;
-  if ($(window).scrollTop() >= $(".section-1").offset().top) {
+  if ($(window).scrollTop() >= $("header").offset().top + $("header").height()) {
     navbar.addClass("sticky");
   } else {
     navbar.removeClass("sticky");
+  }
+
+  if ($(window).scrollTop() + window.innerHeight >= $(".section-2").offset().top) {
+    console.log("yes")
+    stats.removeClass("hidden");
+    stats.addClass("up-in")
   }
 });
