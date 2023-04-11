b3d327d7da085e8ee6a2c125d3f91eeef5776b9e
Jeff Somers <jeff.somers@kbc.be>
2023-04-11 18:08:44 +0200

    first version of section-3

--- app/static/css/main.js
+++ app/static/css/main.js
@@ -23,3 +23,36 @@ $(window).scroll(function () {
     stats.addClass("up-in")
   }
 });
+
+function expand(item) {
+  const content = item.querySelector('.information-content');
+  // Get the height of the content in the expanded state
+  const contentHeight = content.scrollHeight;
+  
+  // Set the height of the div to the height of the content
+  content.style.height = `${contentHeight}px`;
+}
+
+function collapse(item) {
+  const content = item.querySelector('.information-content');
+  // Reset the height of the div to 0
+  content.style.height = '0';
+}
+
+const items = document.querySelectorAll('.information-item');
+
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    if (item.classList.contains('active')) {
+      item.classList.remove('active');
+      collapse(item)
+    } else {
+      items.forEach(item => {
+        item.classList.remove('active');
+        collapse(item)
+      });
+      item.classList.add('active');
+      expand(item)
+    }
+  });
+});
