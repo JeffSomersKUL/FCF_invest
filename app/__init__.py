38f64213d1018787c6823b89eb12bb2a80821f43
Ruben <ruben.stabel@student.kuleuven.be>
2023-12-14 17:11:46 +0100

    ruben_changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+# Flask-Mail Configuration
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'fcfinvest23@gmail.com'
+app.config['MAIL_PASSWORD'] = 'fegva8-borGek-sesvag'
+
+mail = Mail(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from app import app
+from app import app, mail
 import os
-from flask import render_template
+from flask import render_template, request
 from flask import send_from_directory
+from flask_mail import Mail, Message
 
 
 @app.route("/favicon.ico")
@@ -14,6 +15,40 @@ def favicon():
 def index():
     return render_template("index.html")
 
-@app.route("/learn_more")
-def learn_more():
-    return render_template("learn_more.html")
+@app.route("/leadership")
+def leadership():
+    return render_template("leadership.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    email = request.form.get('email')
+    subject = request.form.get('subject')
+    content = request.form.get('content')
+
+    # Process the form data (e.g., save to a database)
+
+    print(f"Form submitted! First Name: {firstname},  Last Name: {lastname}, Email: {email}, Subject: {subject}, Content: {content}")
+
+    send_email(subject, email, 'fcfinvest23@gmail.com', content)
+
+    return render_template("index.html")
+
+def send_email(subject, email, recipient, content):
+    msg = Message(subject, sender=email, recipients=[recipient], body=content)
+
+    try:
+        mail.send(msg)
+        print('Email sent successfully!')
+    except Exception as e:
+        print(f'Error sending email: {e}')
+
