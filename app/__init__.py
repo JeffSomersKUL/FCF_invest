50a783af993964fb9a7eafbbb6a763465e8d1d38
JeffSomersKUL <somersjeff.somers@gmail.com>
2024-05-13 00:03:59 +0200

    new database connection

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,16 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = '7df08e95c64edebe39aef9f41a8e8780'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL') 
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
